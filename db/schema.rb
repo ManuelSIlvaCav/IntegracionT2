0da7d37bebcab2e7a3ca99ae7f1a2824cf6b5f58
Manuel Silva <msilvacav@gmail.com>
2018-04-01 15:49:29 -0300

    del string

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180401175309) do
+ActiveRecord::Schema.define(version: 20180401184634) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "news", force: :cascade do |t|
     t.string   "title"
-    t.string   "string"
     t.text     "subtitle"
     t.text     "body"
     t.datetime "created_at", null: false
