5f4ad9db350ed8dc69d9b4f4460eccdef6cde28a
Manuel Silva <msilvacav@gmail.com>
2018-04-01 15:08:00 -0300

    new

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+noticias = []
+
+30.times do |x|
+  titulo = Faker::Name.name
+  bajada = Faker::Lorem.sentence(10)
+  cuerpo = Faker::Lorem.sentence(rand(300..700))
+  created_at = Faker::Time.between(2.months.ago, Date.today)
+  updated_at = Faker::Time.between(created_at, Date.today)
+  noticias.push(title: titulo, subtitle: bajada, body: cuerpo, created_at: created_at, updated_at: updated_at)
+end
+
+New.create(noticias)
